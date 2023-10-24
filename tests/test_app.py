8b27c041bcd43c48eac82e4fc81d943e08f6a0ba
Romain Grudé <65305184+romaingrude@users.noreply.github.com>
2023-10-24 18:56:46 +0100

    Revert "Rooms Index page and Show page"

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,19 +6,13 @@ from playwright.sync_api import Page, expect
 We can render the index page
 """
 
-def test_get_rooms(db_connection, page, test_web_address):
-    db_connection.seed("seeds/MakersBNB_seed.sql")
-    page.goto(f"http://{test_web_address}/rooms")
-    div_tags = page.locator("div")
-    expect(div_tags).to_have_text([
-        "Name: Room 1\nDescription: This is a room\nPrice: £100.0",
-        "Name: Room 2\nDescription: This is another room\nPrice: £200.0",
-    ])
 
-def test_get_single_room(db_connection, page, test_web_address):
-    db_connection.seed("seeds/MakersBNB_seed.sql")
-    page.goto(f"http://{test_web_address}/rooms/1")
-    div_tag = page.locator("div")
-    h1_tag = page.locator("h1")
-    expect(h1_tag).to_have_text("Room 1")
-    expect(div_tag).to_have_text(["""Description: This is a room\nPrice: £100.0"""])
+def test_get_index(page, test_web_address):
+    # We load a virtual browser and navigate to the /index page
+    page.goto(f"http://{test_web_address}/index")
+
+    # We look at the <p> tag
+    strong_tag = page.locator("p")
+
+    # We assert that it has the text "This is the homepage."
+    expect(strong_tag).to_have_text("This is the homepage.")
