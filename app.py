29a35a7b02f78f9093a220d9a7e9cda56d15ee9a
David Sholoye <david.sholoye@gmail.com>
2023-10-24 16:41:07 +0100

    Rooms Index page and Show page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.rooms import Rooms
+from lib.rooms_repository import RoomsRepository
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -12,10 +14,20 @@ app = Flask(__name__)
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5000/index
-@app.route("/index", methods=["GET"])
-def get_index():
-    return render_template("index.html")
+@app.route("/rooms", methods=["GET"])
+def get_rooms():
+    connection = get_flask_database_connection(app)
+    repo = RoomsRepository(connection)
+    rooms = repo.all()
+    return render_template("rooms/room_index.html", rooms=rooms)
 
+@app.route('/rooms/<id>', methods = ['GET'])
+def get_single_room(id):
+    connection = get_flask_database_connection(app)
+    room_repo = RoomsRepository(connection)
+    room = room_repo.find(id)
+
+    return render_template("rooms/room_show.html", room=room)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
