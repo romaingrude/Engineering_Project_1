9452c09262ac6cb3ebc56f8899974133700a93bd
David Sholoye <david.sholoye@gmail.com>
2023-10-25 10:46:51 +0100

    Resolved Broken tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,16 +9,16 @@ We can render the index page
 def test_get_rooms(db_connection, page, test_web_address):
     db_connection.seed("seeds/MakersBNB_seed.sql")
     page.goto(f"http://{test_web_address}/rooms")
-    div_tags = page.locator("div")
-    expect(div_tags).to_have_text([
+    p_tags = page.locator("p")
+    expect(p_tags).to_have_text([
         "Name: Room 1\nDescription: This is a room\nPrice: £100.0",
-        "Name: Room 2\nDescription: This is another room\nPrice: £200.0",
+        "Name: Room 2\nDescription: This is another room\nPrice: £200.0"
     ])
 
 def test_get_single_room(db_connection, page, test_web_address):
     db_connection.seed("seeds/MakersBNB_seed.sql")
     page.goto(f"http://{test_web_address}/rooms/1")
-    div_tag = page.locator("div")
+    p_tag = page.locator("p")
     h1_tag = page.locator("h1")
     expect(h1_tag).to_have_text("Room 1")
-    expect(div_tag).to_have_text(["""Description: This is a room\nPrice: £100.0"""])
+    expect(p_tag).to_have_text(["""Description: This is a room\nPrice: £100.0"""])
