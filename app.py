0e876eba0702452eb073881959a84b50db1ed4f9
David Sholoye <david.sholoye@gmail.com>
2023-10-25 11:50:39 +0100

    Updated Changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, render_template, session, redirect, url_for
 from lib.database_connection import get_flask_database_connection
 from lib.user_repository import UserRepository
+from lib.rooms import Rooms
+from lib.rooms_repository import RoomsRepository
 import secrets
 
 # Create a new Flask app
@@ -61,9 +63,24 @@ def temp_account():
         # The user is logged in, display their account page.
         return render_template("temp.html")
 
+@app.route("/rooms", methods=["GET"])
+def get_rooms():
+    connection = get_flask_database_connection(app)
+    repo = RoomsRepository(connection)
+    rooms = repo.all()
+    return render_template("rooms/room_index.html", rooms=rooms)
+
+@app.route('/rooms/<id>', methods = ['GET'])
+def get_single_room(id):
+    connection = get_flask_database_connection(app)
+    room_repo = RoomsRepository(connection)
+    room = room_repo.find(id)
+
+    return render_template("rooms/room_show.html", room=room)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == "__main__":
     app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+
